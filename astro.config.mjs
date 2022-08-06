11916876f0fe2764035f8db4fc2499e080eb7b7e
Phúc (Scott) Nguyễn <en.deathraven@gmail.com>
2022-08-06 16:14:45 +0700

    Add Vercel package for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
-	site: `http://astro.build`,
+	output: 'server',
+	adapter: vercel()
 });
