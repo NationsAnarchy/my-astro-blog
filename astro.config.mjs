812201edfe451fa5952083bbc1d2a2953999fd04
Phúc (Scott) Nguyễn <en.deathraven@gmail.com>
2022-08-06 16:30:28 +0700

    Fix errors #2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
+<<<<<<< HEAD
 import vercel from '@astrojs/vercel/serverless'
+=======
+import preact from '@astrojs/preact';
+>>>>>>> parent of 1191687 (Add Vercel package for Astro)
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercel()
+	integrations: [preact()],
+	site: `http://astro.build`,
 });
